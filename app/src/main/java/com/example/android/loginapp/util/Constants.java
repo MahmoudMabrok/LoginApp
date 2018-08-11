2a53896235ad840b4f170fbe152bfd0ac24bd889
Hosam <hossamnasser938@gmail.com>
2018-08-11 08:50:14 +0200

    Add flatMap

--- app/src/main/java/com/example/android/loginapp/util/Constants.java
+++ app/src/main/java/com/example/android/loginapp/util/Constants.java
@@ -5,5 +5,8 @@ public class Constants {
     public static final String BASE_URL = "http://demo3427522.mockable.io/";
     public static final String EMAIL_KEY = "email";
     public static final String PASSWORD_KEY = "password";
+    public static final String TYPE_KEY = "type";
+    public static final String SUCCESS_STATUS = "success";
+    public static final String FAILURE_STATUS = "failure";
 
 }
---
+++ app/src/main/java/com/example/android/loginapp/util/LoginThrowable.java
@@ -0,0 +1,13 @@
+package com.example.android.loginapp.util;
+
+public class LoginThrowable extends Throwable {
+
+    public LoginThrowable(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
